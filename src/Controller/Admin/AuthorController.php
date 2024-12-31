f2789eff82d1e770334ec45052076db2c17588af
jordanetalus <jordanefossitalla@gmail.com>
2024-12-31 17:25:42 +0000

    lecture des objets en bd

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Author;
 use App\Form\AuthorType;
+use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +15,12 @@ use Doctrine\ORM\EntityManagerInterface;
 class AuthorController extends AbstractController
 {
     #[Route('', name: 'app_admin_author_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(AuthorRepository $repository): Response
     {
+        $authors = $repository->findAll();
         return $this->render('admin/author/index.html.twig', [
             'controller_name' => 'AuthorController',
+            'authors' => $authors,
         ]);
     }
 
