a9038b837fd039b54fd42088e42f28ea3cefe02d
jordanetalus <jordanefossitalla@gmail.com>
2025-01-06 14:37:24 +0000

    gestion des autorisation

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -10,10 +10,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted as AttributeIsGranted;
 
 #[Route('/admin/author')]
 class AuthorController extends AbstractController
-{
+{ 
+    #[AttributeIsGranted('IS_AUTHENTICATED')]
     #[Route('', name: 'app_admin_author_index', methods: ['GET'])]
     public function index(AuthorRepository $repository): Response
     {
@@ -31,11 +33,17 @@ class AuthorController extends AbstractController
     ['author'=>$author,]);
     }
 
+    
+  
     #[Route('/{id}/edit', name: 'app_admin_author_edit',requirements: ['id' => '\d+'],
     methods:['GET','POST'])]
     #[Route('/new', name: 'app_admin_author_new', methods: ['GET', 'POST'])]
     public function new(?Author $author,Request $request,EntityManagerInterface $manager): Response
     {
+        if(null == $author)
+        {
+            $this->denyAccessUnlessGranted(attribute:'ROLE_ADMIN');
+        }
         $author ??= new Author();
         $form = $this->createForm(AuthorType::class, $author);
 
